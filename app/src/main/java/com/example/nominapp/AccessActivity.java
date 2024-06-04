3d4809a27bd70df7eb97556838fc7111784f8575
Jon Reyes <jonath.lt3@gmail.com>
2024-06-04 12:35:21 -0700

    Se codifico la clase AccessActivity

--- app/src/main/java/com/example/nominapp/AccessActivity.java
+++ app/src/main/java/com/example/nominapp/AccessActivity.java
@@ -1,4 +1,38 @@
 package com.example.nominapp;
 
-public class AccessActivity {
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AccessActivity extends AppCompatActivity {
+
+    private EditText etNombreAcceso;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_access);
+
+        etNombreAcceso = findViewById(R.id.etNombreAcceso);
+        Button btnIngresar = findViewById(R.id.btnIngresar);
+
+        btnIngresar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!TextUtils.isEmpty(etNombreAcceso.getText())) {
+                    Intent intent = new Intent(AccessActivity.this, MainActivity.class);
+                    intent.putExtra("nombre", etNombreAcceso.getText().toString());
+                    startActivity(intent);
+                } else {
+                    etNombreAcceso.setError("Requerido");
+                }
+            }
+        });
+    }
 }
+
